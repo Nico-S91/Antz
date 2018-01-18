09216265060d5fa58912fc583239e55e478f820a
Nico-S91 <niko.sibi@gmail.com>
2018-01-18 17:18:01 -0300

    Basic Player Movement
    Character can move up and down in a line.

---
+++ Antz/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody2D playerRigidBody;
+    public float speed = 2f;
+
+	// Inicializacion del objeto
+	void Start () {
+		
+	}
+	
+	// Esto se llama una vez por frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, -speed);
+        }
+	}
+}
